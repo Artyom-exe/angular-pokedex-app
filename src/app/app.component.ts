7deead674571c0dfc88ef68cfd239bf1fabb37b2
Artyom-exe <thlambot@hotmail.fr>
2025-02-11 15:26:41 +0100

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-pokedex-app';
+  name = signal('Pikachu');
+  life = signal(21);
+  imageSrc = signal('https://assets.pokemon.com/assets/cms2/img/pokedex/detail/025.png');
+  size = computed(() => {
+    if (this.life() <= 15) {
+      return 'Petit';
+    }
+
+    if (this.life() >= 25) {
+      return 'Grand';
+    }
+
+    return 'Moyen';
+  });
+
+  incrementLife() {
+    this.life.update((n) => n + 1);
+  }
+
+  decrementLife() {
+    this.life.update((n) => n - 1);
+  }
 }
